ee20fe520d6bb9c89db6241a434c9b7371724092
Christopher Edwards <Christopher.Edwards@krminc.com>
2012-08-17 15:06:59 -0400

    Remove check for trailing \
    The check for trailing \ isn't necessary due to the change to
    Path.Combine - Path.Combine takes care of putting in the correct
    separators.  Also this check isn't cross platform compatible.

--- mdo/mdo/src/utils/ResourceUtils.cs
+++ mdo/mdo/src/utils/ResourceUtils.cs
@@ -68,9 +68,8 @@ namespace gov.va.medora.utils
         {
             try
             {
-                string current = System.IO.Path.GetDirectoryName(
-                    Assembly.GetExecutingAssembly().GetName().CodeBase);
-                current = current.Replace("file:\\", "");
+				string current = Directory.GetCurrentDirectory();
+                current = current.Replace("file:", "");
                 return getResources(current);
             }
             catch (Exception)
@@ -102,15 +101,11 @@ namespace gov.va.medora.utils
                 }
                 else // found it!
                 {
-                    if (dirs[0].FullName.Contains("\\bin\\")) // if we're in bin directory, keep recursing up - TBD: should we use this convention?
+                    if (dirs[0].FullName.Contains("bin")) // if we're in bin directory, keep recursing up - TBD: should we use this convention?
                     {
                         return getResources(di.Parent.FullName);
                     }
-                    if (dirs[0].FullName.EndsWith("\\"))
-                    {
-                        return dirs[0].FullName; 
-                    }
-                    else return dirs[0].FullName + "\\";
+					else return dirs[0].FullName;
                 }
             }
             catch (Exception)
